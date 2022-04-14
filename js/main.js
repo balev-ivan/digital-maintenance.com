63470720e74b95d33aa41d2c5efa7c71e4f4c22e
Hasan Gursoy <xfreestyler@gmail.com>
2022-04-14 21:57:36 +0300

    js preparation

--- js/main.js
+++ js/main.js
@@ -1,47 +1,78 @@
 "use strict";
 
 window.onload = function () {
-    alert("hello on load");
+    initCarousel();
+    iniitActiveTab();
 };
 
-// Menu management
+function iniitActiveTab() {
+    const defaultTabId = "home_tab";
 
-// Global variable to store default selection
-let currentSelection = document.querySelector(".menu_info");
+    // Global variable to store default selection
+    let currentSelection = document.getElementById(defaultTabId);
 
-function showDefault() {
+    const navLinks = document.querySelectorAll("nav a");
+
+    // Change active tab
+    function changeActiveTab(targetAnchor) {
+        currentSelection.classList.remove("show-div");
+
+        const selector = targetAnchor.replace("#", "");
+
+        const newTab = document.getElementById(selector);
+
+        currentSelection = newTab;
+        currentSelection.classList.add("show-div");
+    }
+
+    navLinks.forEach(function (e) {
+        e.onclick = function () {
+            const targetAnchor = e.getAttribute("href");
+
+            console.log("targetAnchor", targetAnchor);
+
+            changeActiveTab(targetAnchor);
+        };
+    });
+
+    const hash = window.location.hash;
+    console.log(hash);
+    if (hash.length > 0 && hash.indexOf("_tab") > 0) {
+        // TODO: Load a different tab than default. Please check if such tab exists first. If not fallback to defaultTabId.
+    }
+
+    // Show default
     currentSelection.classList.add("show-div");
 }
 
-// Change active tab
-function changeActiveTab(newSelection) {
-    currentSelection.classList.remove("show-div");
-    let selector = '.' + newSelection;
-    let newTab = document.querySelector(selector);
-    currentSelection = newTab;
-    currentSelection.classList.add("show-div");
-}
+function initCarousel() {
+    const prevButton = document.getElementById("carouselPrev");
+    const nextButton = document.getElementById("carouselNext");
 
-// Carousel management
-let slideIndex = 1;
-showSlides(slideIndex);
+    // Carousel management
+    let slideIndex = 1;
+    showSlides(slideIndex);
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
 
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("carousel_pic");
-    let slideLength = slides.length;
-    if (n > slideLength) {
-        slideIndex = 1
+    function showSlides(n) {
+        let i;
+        let slides = document.getElementsByClassName("carousel_pic");
+        let slideLength = slides.length;
+        if (n > slideLength) {
+            slideIndex = 1
+        }
+        if (n < 1) {
+            slideIndex = slideLength
+        }
+        for (i = 0; i < slideLength; i++) {
+            slides[i].style.display = "none";
+        }
+        slides[slideIndex - 1].style.display = "block";
     }
-    if (n < 1) {
-        slideIndex = slideLength
-    }
-    for (i = 0; i < slideLength; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex - 1].style.display = "block";
+
+    prevButton.onclick = plusSlides(-1);
+    nextButton.onclick = plusSlides(1);
 }
